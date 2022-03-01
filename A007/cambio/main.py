71d1b93f397678c42526da8246b72d697cf18344
filiipake <r.filipake@gmail.com>
2022-03-01 20:03:20 -0300

    CAMBIO

---
+++ A007/cambio/main.py
@@ -0,0 +1,20 @@
+#%%
+#imports
+from datetime import datetime
+import requests
+import json
+
+# %%
+def cotacao(valor):
+    url = 'https://economia.awesomeapi.com.br/json/last/USD-BRL'
+    ret = requests.get(url)
+    dolar = json.loads(ret.text)["USDBRL"]
+    return float(dolar['bid'] * valor)
+
+moeda = cotacao(1)
+print("Cotação atual: {}".format(moeda))
+#%%
+with open('cambio.csv', 'a') as f:
+    f.write(
+        "{};{}\n".format(datetime.strftime(datetime.now(), '%d/%m/%Y %H:%M'), moeda)
+    )
